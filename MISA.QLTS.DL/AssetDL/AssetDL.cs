e0ddaba38b937d94284482a560c7457a4f087f53
dangchien2001 <ndc10012001@gmail.com>
2023-02-26 15:38:05 +0700

    AssetDL

--- MISA.QLTS.DL/AssetDL/AssetDL.cs
+++ MISA.QLTS.DL/AssetDL/AssetDL.cs
@@ -4,6 +4,7 @@ using MISA.QLTS.Common.Constrants;
 using MISA.QLTS.Common.Entities;
 using MISA.QLTS.Common.Entities.DTO;
 using MISA.QLTS.DL.BaseDL;
+using MISA.QLTS.DL.Datacontext;
 using MySqlConnector;
 using System;
 using System.Collections.Generic;
@@ -15,16 +16,67 @@ namespace MISA.QLTS.DL.AssetDL
 {
     public class AssetDL : BaseDL<asset>, IAssetDL
     {
+        /// <summary>
+        /// Hàm xóa nhiều bản ghi
+        /// </summary>
+        /// <param name="assetIds"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public int DeleteAssetMore(List<Guid> assetIds)
         {
-            throw new NotImplementedException();
+            // Khởi tạo câu lệnh sql
+            var sql = "DELETE FROM asset WHERE asset_id IN ('{0}')";
+            var result = 0;
+            using (var mySqlConnection = new MySqlConnection(DataBaseContext.connectionString))
+            {
+                mySqlConnection.Open();
+
+                using (var transaction = mySqlConnection.BeginTransaction())
+                {
+                    try
+                    {
+                        result = mySqlConnection.Execute(string.Format(sql, string.Join("','", assetIds)), transaction: transaction);
+
+                        transaction.Commit();
+                    }
+                    catch (Exception)
+                    {
+                        transaction.Rollback();
+                    }
+                }
+                mySqlConnection.Close();
+            }
+            return result;
         }
 
+        /// <summary>
+        /// Kiểm tra trùng mã tài sản
+        /// </summary>
+        /// <param name="asset"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public List<asset> DuplicateCode(asset asset)
         {
-            throw new NotImplementedException();
+            var storedProcedureName = "Proc_Asset_Duplicate_Code";
+            var parameters = new DynamicParameters();
+            parameters.Add("p_AssetCode", asset.asset_code);
+            parameters.Add("p_AssetId", asset.asset_id);
+            dynamic result;
+            using (var mySqlConnection = new MySqlConnection(Datacontext.DataBaseContext.connectionString))
+            {
+                var multy = mySqlConnection.QueryMultiple(storedProcedureName, parameters, commandType: System.Data.CommandType.StoredProcedure);
+                result = multy.Read<asset>().ToList();
+            }
+            return result;
         }
 
+        /// <summary>
+        /// API lấy danh sách nhân viên lọc theo trang
+        /// </summary>
+        /// <param name="assetFilter"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="pageNumber"></param>
+        /// <returns></returns>
         public PagingResult GetAssetsByFilter(
             [FromQuery] string? assetFilter, 
             [FromQuery] int pageSize = 10, 
@@ -56,6 +108,10 @@ namespace MISA.QLTS.DL.AssetDL
             return result;
         }
 
+        /// <summary>
+        /// Lấy mã nhân viên mới
+        /// </summary>
+        /// <returns></returns>
         public int GetMaxAssetCode()
         {
             var procedureName = "Proc_Asset_MaxCode";
